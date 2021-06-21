a03c284c8349ba8b2a6602003265608624a75a9e
Michelle3334 <michelle.toscano1978@gmail.com>
2021-06-21 12:38:53 +0000

    Add request to user for number of rows and columns, add validation of inputs

--- run.py
+++ run.py
@@ -1,1 +1,45 @@
-# python code goes here
+import random
+
+
+rows = 0
+columns = 0
+board = []
+ship_row = 0
+ship_column = 0
+turns = 0
+
+
+"""
+Welcome message
+"""
+print("Welcome to battleships!")
+
+
+"""
+Request user to select number of rows
+"""
+while True:
+    try:
+        rows = int(input("Please enter the number of rows (4-9).\n"))
+    except ValueError:
+        print("That is not a number! Try again")
+    else:
+        if 4 <= rows < 10:
+            break
+        else:
+            print("Invalid number, please try again.")
+
+
+"""
+Request user to select number of columns
+"""
+while True:
+    try:
+        columns = int(input("Please enter the number of columns(4-9).\n"))
+    except ValueError:
+        print("That is not a number! Try again")
+    else:
+        if 4 <= columns < 10:
+            break
+        else:
+            print("Invalid number, please try again.")
